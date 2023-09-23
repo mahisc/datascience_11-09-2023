8d1033e34ce3715fa20b3e54606c079824547dfd
mahesh <maheshsanjaychivate.rs.bce20@itbhu.ac.in>
2023-09-23 22:26:08 +0530

    u

---
+++ day_01.py
@@ -0,0 +1,56 @@
+# First line of code 
+print("hello IIT BHU❤")
+
+# addition
+print(5+10)
+
+# print stars
+# spaces inside quotations counts 
+print(" * ")
+print(" ** ")
+print(" *** ")
+print("****")
+print(" ** ")
+print(" * ")
+
+# addition
+print(1+1)
+
+# substraction
+print(2-2)
+
+# multiply
+print(3*2)
+
+# divide
+print(10/5)
+
+#power
+print(10**2)
+
+# floor divide
+print(7//3)
+
+# remainder
+print(7%3)
+
+# connect different lines of codes 'variables'
+result= 10+5
+print(result)
+
+result2=result/3
+print(result2)
+
+# always be careful while naming variables;be consistent.
+
+feature_1_model= 30
+feature_1_model+=20
+print(feature_1_model)
+    # functions 
+ 
+print('test')
+print(len('mahesh is a boy'))
+# another way
+word_count=len('mahesh is a smart boy')
+print(word_count)
+"""this will be a multi-line # comment"""
